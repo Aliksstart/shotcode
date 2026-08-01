65d74103f02b16cdcfa0c661562d058b45523797
Aliksstart <aliksstart@mail.ru>
2026-08-02 00:26:41 +0700

    feat: created winforms.

---
+++ src/WinForms/MainWindow.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace WinForms
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            WizzardPanel = new Panel();
+            pictureBox1 = new PictureBox();
+            WizzardPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // WizzardPanel
+            // 
+            WizzardPanel.Controls.Add(pictureBox1);
+            WizzardPanel.Dock = DockStyle.Fill;
+            WizzardPanel.Location = new Point(0, 0);
+            WizzardPanel.Name = "WizzardPanel";
+            WizzardPanel.Size = new Size(584, 361);
+            WizzardPanel.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Dock = DockStyle.Top;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(584, 520);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // MainWindow
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(584, 361);
+            Controls.Add(WizzardPanel);
+            Name = "MainWindow";
+            Text = "ShotCode";
+            WizzardPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel WizzardPanel;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ src/WinForms/MainWindow.cs
@@ -0,0 +1,10 @@
+namespace WinForms
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
