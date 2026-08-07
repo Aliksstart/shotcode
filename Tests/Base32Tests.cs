c23e4172b3d5209b728398a5495e2cac2ab2c84a
Aliksstart <aliksstart@mail.ru>
2026-08-07 22:37:05 +0700

    feat: Added the base32 algorithm.

---
+++ Tests/Base32Tests.cs
@@ -0,0 +1,162 @@
+﻿using Core.Crypto;
+using System.Text;
+
+namespace Tests.Crypto
+{
+    [TestClass]
+    public class Base32Tests
+    {
+        /*
+        BASE32("") = ""
+        BASE32("f") = "MY======"
+        BASE32("fo") = "MZXQ===="
+        BASE32("foo") = "MZXW6==="
+        BASE32("foob") = "MZXW6YQ="
+        BASE32("fooba") = "MZXW6YTB"
+        BASE32("foobar") = "MZXW6YTBOI======"
+        */
+        byte[] r_0 = { };
+        byte[] r_1 = Encoding.UTF8.GetBytes("f");
+        byte[] r_2 = Encoding.UTF8.GetBytes("fo");
+        byte[] r_3 = Encoding.UTF8.GetBytes("foo");
+        byte[] r_4 = Encoding.UTF8.GetBytes("foob");
+        byte[] r_5 = Encoding.UTF8.GetBytes("fooba");
+        byte[] r_6 = Encoding.UTF8.GetBytes("foobar");
+        string l_0 = "";
+        string l_1 = "MY======";
+        string l_2 = "MZXQ====";
+        string l_3 = "MZXW6===";
+        string l_4 = "MZXW6YQ=";
+        string l_5 = "MZXW6YTB";
+        string l_6 = "MZXW6YTBOI======";
+
+        private void EncHelper(byte[] a, string b)
+        {
+            Span<char> dst = stackalloc char[Base32.GetEncodeLength(a.Length)];
+            Base32.Encode(a, dst);
+            Assert.AreEqual(b, dst.ToString());
+        }
+        private void DecHelper(string a, byte[]? b)
+        {
+            Span<byte> dst = stackalloc byte[Base32.GetDecodeLength(a)];
+            Base32.Decode(a, dst);
+            if (b != null)
+            {
+                CollectionAssert.AreEqual(b, dst.ToArray());
+            }
+        }
+        [TestMethod]
+        public void StandartEncodeTest()
+        {
+            EncHelper(r_0, l_0);
+            EncHelper(r_1, l_1);
+            EncHelper(r_2, l_2);
+            EncHelper(r_3, l_3);
+            EncHelper(r_4, l_4);
+            EncHelper(r_5, l_5);
+            EncHelper(r_6, l_6);
+        }
+        [TestMethod]
+        public void StandartDecodeTest()
+        {
+            DecHelper(l_0, r_0);
+            DecHelper(l_1, r_1);
+            DecHelper(l_2, r_2);
+            DecHelper(l_3, r_3);
+            DecHelper(l_4, r_4);
+            DecHelper(l_5, r_5);
+            DecHelper(l_6, r_6);
+        }
+        [TestMethod]
+        public void RoundTripTest()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                int s = rnd.Next(32, 1024);
+                byte[] d = new byte[s];
+                rnd.NextBytes(d);
+                Span<char> d_enc = stackalloc char[Base32.GetEncodeLength(d.Length)];
+                Base32.Encode(d, d_enc);
+                Span<byte> dec = stackalloc byte[d.Length];
+                Base32.Decode(d_enc, dec);
+                CollectionAssert.AreEqual(d, dec.ToArray());
+            }
+        }
+        [TestMethod]
+        public void InvalidDataExceptionGetDecodeLenghtTest()
+        {
+            Assert.ThrowsException<InvalidDataException>(() => {
+                string str = "MZXQ==AR=";
+                Base32.GetDecodeLength(str);
+            });
+            Assert.ThrowsException<InvalidDataException>(() => {
+                string str = "MZXQ=A==";
+                Base32.GetDecodeLength(str);
+            });
+            Assert.ThrowsException<InvalidDataException>(() => {
+                string str = "MZXQ==";
+                Base32.GetDecodeLength(str);
+            });
+            Assert.ThrowsException<InvalidDataException>(() => {
+                string str = "MZXQ=====";
+                Base32.GetDecodeLength(str);
+            });
+            Assert.ThrowsException<InvalidDataException>(() => {
+                string str = "MZXQ====\n";
+                Base32.GetDecodeLength(str);
+            });
+            Assert.ThrowsException<InvalidDataException>(() => {
+                string str = "MZX=====";
+                Base32.GetDecodeLength(str);
+            });
+            Assert.ThrowsException<InvalidDataException>(() => {
+                string str = "M=======";
+                Base32.GetDecodeLength(str);
+            });
+            Assert.ThrowsException<InvalidDataException>(() => {
+                string str = "========";
+                Base32.GetDecodeLength(str);
+            });
+        }
+        [TestMethod]
+        public void InvalidDataExceptionDecodeTest()
+        {
+            Assert.ThrowsException<InvalidDataException>(() => {
+                string str = "";
+                Span<byte> d = stackalloc byte[4];
+                Base32.Decode(str.AsSpan(), d);
+            });
+            Assert.ThrowsException<InvalidDataException>(() => {
+                string str = "MY======";
+                Span<byte> d = stackalloc byte[0];
+                Base32.Decode(str.AsSpan(), d);
+            });
+            Assert.ThrowsException<InvalidDataException>(() => {
+                string str = "MZXQ====MY======";
+                Span<byte> d = stackalloc byte[0];
+                Base32.Decode(str.AsSpan(), d);
+            });
+            Assert.ThrowsException<InvalidDataException>(() => {
+                string str = "MZXQ====MY======";
+                Span<byte> d = stackalloc byte[Base32.GetDecodeLength(str)];
+                Base32.Decode(str, d);
+            });
+            Assert.ThrowsException<InvalidDataException>(() => {
+                string str = "MZXQ==A=";
+                Span<byte> d = stackalloc byte[Base32.GetDecodeLength(str)];
+                Base32.Decode(str, d);
+            });
+            Assert.ThrowsException<InvalidDataException>(() => {
+                string str = "mzxq====";
+                Span<byte> d = stackalloc byte[Base32.GetDecodeLength(str)];
+                Base32.Decode(str, d);
+            });
+            Assert.ThrowsException<InvalidDataException>(() => {
+                string str = "MZXR====";
+                Span<byte> d = stackalloc byte[Base32.GetDecodeLength(str)];
+                Base32.Decode(str, d);
+            });
+        }
+    }
+}
---
+++ src/Core/Crypto/Base32.cs
@@ -0,0 +1,125 @@
+﻿namespace Core.Crypto
+{
+    public static class Base32
+    {
+        private const string Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
+
+        private static readonly int[] ReverseAlphabet = CreateReverseLookup();
+        private static int[] CreateReverseLookup()
+        {
+            var reverse = new int[128];
+            Array.Fill(reverse, -1);
+            for (int i = 0; i < Alphabet.Length; i++)
+            {
+                reverse[Alphabet[i]] = i;
+            }
+            return reverse;
+        }
+
+        private static int GetSignificantLength(ReadOnlySpan<char> src)
+        {
+            if (src.Length % 8 != 0)
+                throw new InvalidDataException($"Invalid size {nameof(src)}");
+            int end = 0;
+            for (int i = src.Length - 1; i >= 0; --i)
+            {
+                if (src[i] != '=')
+                {
+                    end = 1 + i;
+                    break;
+                }
+            }
+            int pad = src.Length - end;
+            if (pad != 0 && pad != 1 && pad != 3 && pad != 4 && pad != 6)
+                throw new InvalidDataException("Inaccessible padding.");
+            return end;
+        }
+
+        public static void Decode(ReadOnlySpan<char> src, Span<byte> dst)
+        {
+            if (src.Length == 0)
+            {
+                if (dst.Length != 0)
+                    throw new InvalidDataException($"{nameof(dst)} invalid size");
+                return;
+            }
+            int end = GetSignificantLength(src);
+            if (dst.Length != end * 5 / 8)
+                throw new InvalidDataException($"{nameof(dst)} size invalid.");
+            int curr = 0;
+            dst.Clear();
+            while (curr < end * 5)
+            {
+                int el = curr / 5;
+                int index = curr / 8;
+                int offset = curr % 8;
+                int i = ReverseAlphabet[src[el]];
+                if (i == -1)
+                    throw new InvalidDataException($"The symbol '{src[el]}' was not found in the alphabet.");
+                if (8 - (5 + offset) >= 0)
+                {
+                    dst[index] |= (byte)(i << (8 - (5 + offset)));
+                }
+                else
+                {
+                    int k = ((-1) * (8 - (5 + offset)));
+                    dst[index] |= (byte)(i >> k);
+                    if (index + 1 < dst.Length)
+                    {
+                        dst[index + 1] |= (byte)(i << (8 - k));
+                    }
+                    else if ((i & (1 << k) - 1) != 0)
+                    {
+                        throw new InvalidDataException("Non-zero padding bits in the last symbol.");
+                    }
+                }
+
+                curr += 5;
+            }
+        }
+
+        public static void Encode(ReadOnlySpan<byte> src, Span<char> dst)
+        {
+            if (src.Length == 0)
+            {
+                if (dst.Length != 0)
+                    throw new InvalidDataException($"{nameof(dst)} invalid size");
+                return;
+            }
+            int required_length = GetEncodeLength(src.Length);
+            if (dst.Length != required_length)
+                throw new InvalidDataException($"{nameof(dst)} size invalid.");
+            dst.Fill('=');
+            int totalBits = src.Length * 8;
+            int currStart = 0;
+            int res_el = 0;
+            while (currStart < totalBits)
+            {
+                int index = currStart / 8;
+                int offset = currStart % 8;
+                int value;
+
+                if (offset <= 3)
+                {
+                    value = (src[index] >> (3 - offset)) & 0x1F;
+                }
+                else
+                {
+                    int hi = (src[index] << (offset - 3)) & 0x1F;
+                    int lo = index + 1 < src.Length ? src[index + 1] >> (11 - offset) : 0;
+                    value = (hi | lo) & 0x1F;
+                }
+                dst[res_el] = Alphabet[value];
+                res_el++;
+                currStart += 5;
+            }
+
+        }
+
+        public static int GetEncodeLength(int byteCount)
+        {
+            return ((byteCount + 4) / 5) * 8;
+        }
+        public static int GetDecodeLength(ReadOnlySpan<char> src) => src.Length == 0 ? 0 : GetSignificantLength(src) * 5 / 8;
+    }
+}
