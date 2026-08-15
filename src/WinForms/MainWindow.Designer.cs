56c26775a3bc1484376b3f81a24ba27e109b0ea2
Aliksstart <aliksstart@mail.ru>
2026-08-16 00:39:17 +0700

    feat: The UI now features the ability to create and open a database, add secrets, and view the database.

--- src/WinForms/MainWindow.Designer.cs
+++ src/WinForms/MainWindow.Designer.cs
@@ -28,49 +28,381 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
             WizzardPanel = new Panel();
-            pictureBox1 = new PictureBox();
+            WizzardSplitContainer = new SplitContainer();
+            PathToFileTextBox = new TextBox();
+            CreateButton = new Button();
+            PasswordBox = new TextBox();
+            ExitButton = new Button();
+            OpenButton = new Button();
+            LogoPictureBox = new PictureBox();
+            VaultPanel = new Panel();
+            SecretsListView = new ListView();
+            NameService = new ColumnHeader();
+            CodeService = new ColumnHeader();
+            LeftTime = new ColumnHeader();
+            AddedPanel = new Panel();
+            AddedSecButton = new Button();
+            AddInfoPanel = new Panel();
+            ServiceNameTextBox = new TextBox();
+            ServiceNameLabel = new Label();
+            CodeAddInfoTextBox = new TextBox();
+            CodeAddInfoLabel = new Label();
+            PeriodAddInfoNumericUpDown = new NumericUpDown();
+            DigitsAddInfoNumericUpDown = new NumericUpDown();
+            AlgorithmAddInfoComboBox = new ComboBox();
+            PeriodAddInfoLabel = new Label();
+            DigitsAddInfoLabel = new Label();
+            AlgorithmAddInfoLabel = new Label();
+            AddInfoActionPanel = new Panel();
+            CancelAddInfoButton = new Button();
+            addedSecInfoButton = new Button();
             WizzardPanel.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)WizzardSplitContainer).BeginInit();
+            WizzardSplitContainer.Panel1.SuspendLayout();
+            WizzardSplitContainer.Panel2.SuspendLayout();
+            WizzardSplitContainer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)LogoPictureBox).BeginInit();
+            VaultPanel.SuspendLayout();
+            AddedPanel.SuspendLayout();
+            AddInfoPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)PeriodAddInfoNumericUpDown).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)DigitsAddInfoNumericUpDown).BeginInit();
+            AddInfoActionPanel.SuspendLayout();
             SuspendLayout();
             // 
             // WizzardPanel
             // 
-            WizzardPanel.Controls.Add(pictureBox1);
-            WizzardPanel.Dock = DockStyle.Fill;
-            WizzardPanel.Location = new Point(0, 0);
+            WizzardPanel.Controls.Add(WizzardSplitContainer);
+            WizzardPanel.Controls.Add(LogoPictureBox);
+            WizzardPanel.Enabled = false;
+            WizzardPanel.Location = new Point(42, 12);
             WizzardPanel.Name = "WizzardPanel";
-            WizzardPanel.Size = new Size(584, 361);
+            WizzardPanel.Size = new Size(584, 341);
             WizzardPanel.TabIndex = 0;
+            WizzardPanel.Visible = false;
             // 
-            // pictureBox1
+            // WizzardSplitContainer
             // 
-            pictureBox1.Dock = DockStyle.Top;
-            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(0, 0);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(584, 520);
-            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
-            pictureBox1.TabIndex = 0;
-            pictureBox1.TabStop = false;
+            WizzardSplitContainer.Dock = DockStyle.Fill;
+            WizzardSplitContainer.Location = new Point(0, 201);
+            WizzardSplitContainer.Name = "WizzardSplitContainer";
+            WizzardSplitContainer.Orientation = Orientation.Horizontal;
+            // 
+            // WizzardSplitContainer.Panel1
+            // 
+            WizzardSplitContainer.Panel1.Controls.Add(PathToFileTextBox);
+            WizzardSplitContainer.Panel1.Controls.Add(CreateButton);
+            WizzardSplitContainer.Panel1.Controls.Add(PasswordBox);
+            // 
+            // WizzardSplitContainer.Panel2
+            // 
+            WizzardSplitContainer.Panel2.Controls.Add(ExitButton);
+            WizzardSplitContainer.Panel2.Controls.Add(OpenButton);
+            WizzardSplitContainer.Size = new Size(584, 140);
+            WizzardSplitContainer.SplitterDistance = 64;
+            WizzardSplitContainer.TabIndex = 1;
+            // 
+            // PathToFileTextBox
+            // 
+            PathToFileTextBox.Location = new Point(93, 36);
+            PathToFileTextBox.Name = "PathToFileTextBox";
+            PathToFileTextBox.Size = new Size(442, 23);
+            PathToFileTextBox.TabIndex = 2;
+            PathToFileTextBox.Text = "Path to file";
+            // 
+            // CreateButton
+            // 
+            CreateButton.Location = new Point(12, 35);
+            CreateButton.Name = "CreateButton";
+            CreateButton.Size = new Size(75, 23);
+            CreateButton.TabIndex = 1;
+            CreateButton.Text = "Create";
+            CreateButton.UseVisualStyleBackColor = true;
+            CreateButton.Click += CreateButton_Click;
+            // 
+            // PasswordBox
+            // 
+            PasswordBox.Location = new Point(12, 6);
+            PasswordBox.Name = "PasswordBox";
+            PasswordBox.Size = new Size(560, 23);
+            PasswordBox.TabIndex = 0;
+            PasswordBox.Text = "Password";
+            // 
+            // ExitButton
+            // 
+            ExitButton.Location = new Point(399, 23);
+            ExitButton.Name = "ExitButton";
+            ExitButton.Size = new Size(75, 23);
+            ExitButton.TabIndex = 1;
+            ExitButton.Text = "Exit";
+            ExitButton.UseVisualStyleBackColor = true;
+            ExitButton.Click += ExitButton_Click;
+            // 
+            // OpenButton
+            // 
+            OpenButton.Location = new Point(107, 23);
+            OpenButton.Name = "OpenButton";
+            OpenButton.Size = new Size(75, 23);
+            OpenButton.TabIndex = 0;
+            OpenButton.Text = "Open";
+            OpenButton.UseVisualStyleBackColor = true;
+            OpenButton.Click += OpenButton_Click;
+            // 
+            // LogoPictureBox
+            // 
+            LogoPictureBox.Dock = DockStyle.Top;
+            LogoPictureBox.Image = Properties.Resources.shx;
+            LogoPictureBox.Location = new Point(0, 0);
+            LogoPictureBox.Name = "LogoPictureBox";
+            LogoPictureBox.Size = new Size(584, 201);
+            LogoPictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            LogoPictureBox.TabIndex = 0;
+            LogoPictureBox.TabStop = false;
+            // 
+            // VaultPanel
+            // 
+            VaultPanel.Controls.Add(SecretsListView);
+            VaultPanel.Controls.Add(AddedPanel);
+            VaultPanel.Location = new Point(276, 23);
+            VaultPanel.Name = "VaultPanel";
+            VaultPanel.Size = new Size(293, 306);
+            VaultPanel.TabIndex = 1;
+            // 
+            // SecretsListView
+            // 
+            SecretsListView.Columns.AddRange(new ColumnHeader[] { NameService, CodeService, LeftTime });
+            SecretsListView.Dock = DockStyle.Fill;
+            SecretsListView.Location = new Point(0, 0);
+            SecretsListView.Name = "SecretsListView";
+            SecretsListView.ShowItemToolTips = true;
+            SecretsListView.Size = new Size(293, 274);
+            SecretsListView.TabIndex = 1;
+            SecretsListView.UseCompatibleStateImageBehavior = false;
+            SecretsListView.View = View.Details;
+            // 
+            // NameService
+            // 
+            NameService.Text = "Service";
+            // 
+            // CodeService
+            // 
+            CodeService.Text = "Code";
+            // 
+            // LeftTime
+            // 
+            LeftTime.Text = "Left";
+            // 
+            // AddedPanel
+            // 
+            AddedPanel.Controls.Add(AddedSecButton);
+            AddedPanel.Dock = DockStyle.Bottom;
+            AddedPanel.Location = new Point(0, 274);
+            AddedPanel.Name = "AddedPanel";
+            AddedPanel.Size = new Size(293, 32);
+            AddedPanel.TabIndex = 0;
+            // 
+            // AddedSecButton
+            // 
+            AddedSecButton.Location = new Point(63, 9);
+            AddedSecButton.Name = "AddedSecButton";
+            AddedSecButton.Size = new Size(75, 23);
+            AddedSecButton.TabIndex = 0;
+            AddedSecButton.Text = "Added";
+            AddedSecButton.UseVisualStyleBackColor = true;
+            AddedSecButton.Click += AddedSecButton_Click;
+            // 
+            // AddInfoPanel
+            // 
+            AddInfoPanel.Controls.Add(ServiceNameTextBox);
+            AddInfoPanel.Controls.Add(ServiceNameLabel);
+            AddInfoPanel.Controls.Add(CodeAddInfoTextBox);
+            AddInfoPanel.Controls.Add(CodeAddInfoLabel);
+            AddInfoPanel.Controls.Add(PeriodAddInfoNumericUpDown);
+            AddInfoPanel.Controls.Add(DigitsAddInfoNumericUpDown);
+            AddInfoPanel.Controls.Add(AlgorithmAddInfoComboBox);
+            AddInfoPanel.Controls.Add(PeriodAddInfoLabel);
+            AddInfoPanel.Controls.Add(DigitsAddInfoLabel);
+            AddInfoPanel.Controls.Add(AlgorithmAddInfoLabel);
+            AddInfoPanel.Controls.Add(AddInfoActionPanel);
+            AddInfoPanel.Location = new Point(12, 113);
+            AddInfoPanel.Name = "AddInfoPanel";
+            AddInfoPanel.Size = new Size(370, 281);
+            AddInfoPanel.TabIndex = 2;
+            // 
+            // ServiceNameTextBox
+            // 
+            ServiceNameTextBox.Location = new Point(73, 9);
+            ServiceNameTextBox.Name = "ServiceNameTextBox";
+            ServiceNameTextBox.Size = new Size(120, 23);
+            ServiceNameTextBox.TabIndex = 10;
+            // 
+            // ServiceNameLabel
+            // 
+            ServiceNameLabel.AutoSize = true;
+            ServiceNameLabel.Location = new Point(3, 12);
+            ServiceNameLabel.Name = "ServiceNameLabel";
+            ServiceNameLabel.Size = new Size(47, 15);
+            ServiceNameLabel.TabIndex = 9;
+            ServiceNameLabel.Text = "Service:";
+            // 
+            // CodeAddInfoTextBox
+            // 
+            CodeAddInfoTextBox.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            CodeAddInfoTextBox.Location = new Point(73, 125);
+            CodeAddInfoTextBox.Multiline = true;
+            CodeAddInfoTextBox.Name = "CodeAddInfoTextBox";
+            CodeAddInfoTextBox.Size = new Size(294, 118);
+            CodeAddInfoTextBox.TabIndex = 8;
+            // 
+            // CodeAddInfoLabel
+            // 
+            CodeAddInfoLabel.AutoSize = true;
+            CodeAddInfoLabel.Location = new Point(3, 128);
+            CodeAddInfoLabel.Name = "CodeAddInfoLabel";
+            CodeAddInfoLabel.Size = new Size(38, 15);
+            CodeAddInfoLabel.TabIndex = 7;
+            CodeAddInfoLabel.Text = "Code:";
+            // 
+            // PeriodAddInfoNumericUpDown
+            // 
+            PeriodAddInfoNumericUpDown.Location = new Point(72, 96);
+            PeriodAddInfoNumericUpDown.Name = "PeriodAddInfoNumericUpDown";
+            PeriodAddInfoNumericUpDown.Size = new Size(120, 23);
+            PeriodAddInfoNumericUpDown.TabIndex = 6;
+            // 
+            // DigitsAddInfoNumericUpDown
+            // 
+            DigitsAddInfoNumericUpDown.Location = new Point(72, 67);
+            DigitsAddInfoNumericUpDown.Name = "DigitsAddInfoNumericUpDown";
+            DigitsAddInfoNumericUpDown.Size = new Size(121, 23);
+            DigitsAddInfoNumericUpDown.TabIndex = 5;
+            // 
+            // AlgorithmAddInfoComboBox
+            // 
+            AlgorithmAddInfoComboBox.FormattingEnabled = true;
+            AlgorithmAddInfoComboBox.Location = new Point(73, 38);
+            AlgorithmAddInfoComboBox.Name = "AlgorithmAddInfoComboBox";
+            AlgorithmAddInfoComboBox.Size = new Size(121, 23);
+            AlgorithmAddInfoComboBox.TabIndex = 4;
+            // 
+            // PeriodAddInfoLabel
+            // 
+            PeriodAddInfoLabel.AutoSize = true;
+            PeriodAddInfoLabel.Location = new Point(3, 98);
+            PeriodAddInfoLabel.Name = "PeriodAddInfoLabel";
+            PeriodAddInfoLabel.Size = new Size(44, 15);
+            PeriodAddInfoLabel.TabIndex = 3;
+            PeriodAddInfoLabel.Text = "Period:";
+            // 
+            // DigitsAddInfoLabel
+            // 
+            DigitsAddInfoLabel.AutoSize = true;
+            DigitsAddInfoLabel.Location = new Point(3, 69);
+            DigitsAddInfoLabel.Name = "DigitsAddInfoLabel";
+            DigitsAddInfoLabel.Size = new Size(40, 15);
+            DigitsAddInfoLabel.TabIndex = 2;
+            DigitsAddInfoLabel.Text = "Digits:";
+            // 
+            // AlgorithmAddInfoLabel
+            // 
+            AlgorithmAddInfoLabel.AutoSize = true;
+            AlgorithmAddInfoLabel.Location = new Point(3, 41);
+            AlgorithmAddInfoLabel.Name = "AlgorithmAddInfoLabel";
+            AlgorithmAddInfoLabel.Size = new Size(64, 15);
+            AlgorithmAddInfoLabel.TabIndex = 1;
+            AlgorithmAddInfoLabel.Text = "Algorithm:";
+            // 
+            // AddInfoActionPanel
+            // 
+            AddInfoActionPanel.Controls.Add(CancelAddInfoButton);
+            AddInfoActionPanel.Controls.Add(addedSecInfoButton);
+            AddInfoActionPanel.Dock = DockStyle.Bottom;
+            AddInfoActionPanel.Location = new Point(0, 249);
+            AddInfoActionPanel.Name = "AddInfoActionPanel";
+            AddInfoActionPanel.Size = new Size(370, 32);
+            AddInfoActionPanel.TabIndex = 0;
+            // 
+            // CancelAddInfoButton
+            // 
+            CancelAddInfoButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            CancelAddInfoButton.Location = new Point(292, 6);
+            CancelAddInfoButton.Name = "CancelAddInfoButton";
+            CancelAddInfoButton.Size = new Size(75, 23);
+            CancelAddInfoButton.TabIndex = 1;
+            CancelAddInfoButton.Text = "Cancel";
+            CancelAddInfoButton.UseVisualStyleBackColor = true;
+            CancelAddInfoButton.Click += CancelAddInfoButton_Click;
+            // 
+            // addedSecInfoButton
+            // 
+            addedSecInfoButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            addedSecInfoButton.Location = new Point(3, 6);
+            addedSecInfoButton.Name = "addedSecInfoButton";
+            addedSecInfoButton.Size = new Size(75, 23);
+            addedSecInfoButton.TabIndex = 0;
+            addedSecInfoButton.Text = "Added";
+            addedSecInfoButton.UseVisualStyleBackColor = true;
+            addedSecInfoButton.Click += addedSecInfoButton_Click;
             // 
             // MainWindow
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(584, 361);
+            ClientSize = new Size(584, 341);
+            Controls.Add(AddInfoPanel);
+            Controls.Add(VaultPanel);
             Controls.Add(WizzardPanel);
+            FormBorderStyle = FormBorderStyle.None;
             Name = "MainWindow";
             Text = "ShotCode";
             WizzardPanel.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            WizzardSplitContainer.Panel1.ResumeLayout(false);
+            WizzardSplitContainer.Panel1.PerformLayout();
+            WizzardSplitContainer.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)WizzardSplitContainer).EndInit();
+            WizzardSplitContainer.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)LogoPictureBox).EndInit();
+            VaultPanel.ResumeLayout(false);
+            AddedPanel.ResumeLayout(false);
+            AddInfoPanel.ResumeLayout(false);
+            AddInfoPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)PeriodAddInfoNumericUpDown).EndInit();
+            ((System.ComponentModel.ISupportInitialize)DigitsAddInfoNumericUpDown).EndInit();
+            AddInfoActionPanel.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
 
         private Panel WizzardPanel;
-        private PictureBox pictureBox1;
+        private PictureBox LogoPictureBox;
+        private SplitContainer WizzardSplitContainer;
+        private TextBox PathToFileTextBox;
+        private Button CreateButton;
+        private TextBox PasswordBox;
+        private Button ExitButton;
+        private Button OpenButton;
+        private Panel VaultPanel;
+        private Panel AddedPanel;
+        private Button AddedSecButton;
+        private ListView SecretsListView;
+        private ColumnHeader NameService;
+        private Panel AddInfoPanel;
+        private Panel AddInfoActionPanel;
+        private Button CancelAddInfoButton;
+        private Button addedSecInfoButton;
+        private Label AlgorithmAddInfoLabel;
+        private Label DigitsAddInfoLabel;
+        private Label PeriodAddInfoLabel;
+        private NumericUpDown DigitsAddInfoNumericUpDown;
+        private ComboBox AlgorithmAddInfoComboBox;
+        private Label CodeAddInfoLabel;
+        private NumericUpDown PeriodAddInfoNumericUpDown;
+        private TextBox CodeAddInfoTextBox;
+        private Label ServiceNameLabel;
+        private TextBox ServiceNameTextBox;
+        private ColumnHeader CodeService;
+        private ColumnHeader LeftTime;
     }
 }
--- src/WinForms/MainWindow.cs
+++ src/WinForms/MainWindow.cs
@@ -1,10 +1,197 @@
+using Core;
+using Core.Crypto;
+using System.Security.Cryptography;
+using System.Text;
+
 namespace WinForms
 {
     public partial class MainWindow : Form
     {
+        private enum Screen { Wizzard, Valut, AddInfo };
+        private SCDB? _db;
+        private VaultOrigin _origin;
+        private const string scdbFiletr = "ShotCode vault (*.scdb)|*.scdb|All files (*.*)|*.*";
+        private int _timeout = 60000;
+
+        private void ShowScreen(Screen s)
+        {
+            switch (s)
+            {
+                case Screen.Wizzard:
+                    _db?.Dispose();
+                    _db = null;
+                    break;
+                case Screen.Valut:
+                    SecretsListView.Items.Clear();
+                    int skip_block = 0;
+                    foreach (var e in _origin.GetNameServices())
+                    {
+                        var item = new ListViewItem(e);
+                        try
+                        {
+                            string code = _origin.GetCodeString(e);
+                            item.SubItems.Add(code);
+                        }
+                        catch (Exception ex)
+                        {
+                            skip_block++;
+                            item.ToolTipText = ex.Message;
+                            item.SubItems.Add("---");
+                        }
+                        item.SubItems.Add("TODO");
+                        SecretsListView.Items.Add(item);
+                    }
+                    if (skip_block > 0)
+                    {
+                        MessageBox.Show($"Skiped block: {skip_block}");
+                    }
+                    break;
+                case Screen.AddInfo:
+                    break;
+            }
+            SetPanel(WizzardPanel, s == Screen.Wizzard);
+            SetPanel(VaultPanel, s == Screen.Valut);
+            SetPanel(AddInfoPanel, s == Screen.AddInfo);
+        }
+        private void SetPanel(Panel p, bool on)
+        {
+            p.Visible = p.Enabled = on;
+            p.Dock = on ? DockStyle.Fill : DockStyle.None;
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            SecretsListView.View = View.Details;
+            PeriodAddInfoNumericUpDown.Minimum = Core.Crypto.Constraints.Totp.MinPeriodSeconds;
+            PeriodAddInfoNumericUpDown.Maximum = Core.Crypto.Constraints.Totp.MaxPeriodSeconds;
+            DigitsAddInfoNumericUpDown.Minimum = Core.Crypto.Constraints.Totp.MinDigits;
+            DigitsAddInfoNumericUpDown.Maximum = Core.Crypto.Constraints.Totp.MaxDigits;
+            AlgorithmAddInfoComboBox.Items.AddRange(Enum.GetNames(typeof(Core.Crypto.AlgorithmType)));
+            ShowScreen(Screen.Wizzard);
+        }
+
+        private bool OpenOrCreateDB(string path)
+        {
+            if (_db != null)
+                return false;
+            try
+            {
+                _db = new SCDB(path);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private bool TryOpenVault(string path, bool create)
+        {
+            if (!OpenOrCreateDB(path)) { MessageBox.Show("Can't open file"); return false; }
+            byte[] pass = Encoding.UTF8.GetBytes(PasswordBox.Text);
+            try
+            {
+                _origin = create
+                    ? new VaultOrigin(_db, ref pass, KdfType.Argon2id, _timeout, OnClosedDb)
+                    : new VaultOrigin(_db, ref pass, _timeout, OnClosedDb);
+                if (create) _origin.Save();
+                return true;
+            }
+            catch (VaultDecryptionException) { MessageBox.Show("The password is incorrect, or the file is corrupted."); }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+            finally { CryptographicOperations.ZeroMemory(pass); }
+            _db?.Dispose(); _db = null;
+            return false;
+        }
+
+        private void OnClosedDb()
+        {
+
+            if (WizzardPanel.InvokeRequired)
+            {
+                WizzardPanel.Invoke(() => { OnClosedDb(); });
+            }
+            else
+            {
+                MessageBox.Show("DB closed");
+                ShowScreen(Screen.Wizzard);
+            }
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void CreateButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog()
+            {
+                Filter = scdbFiletr
+            };
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                string path = sfd.FileName;
+                if (File.Exists(path))
+                {
+                    MessageBox.Show("File is exists");
+                    return;
+                }
+                if (TryOpenVault(path, true))
+                {
+                    ShowScreen(Screen.Valut);
+                }
+            }
+        }
+
+        private void OpenButton_Click(object sender, EventArgs e)
+        {
+            if (_db == null)
+            {
+                OpenFileDialog ofd = new OpenFileDialog()
+                {
+                    Filter = scdbFiletr
+                };
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    string path = ofd.FileName;
+                    if (TryOpenVault(path, false))
+                    {
+                        ShowScreen(Screen.Valut);
+                    }
+                }
+            }
+        }
+
+        private void AddedSecButton_Click(object sender, EventArgs e)
+        {
+            ShowScreen(Screen.AddInfo);
+        }
+
+        private void CancelAddInfoButton_Click(object sender, EventArgs e)
+        {
+            ShowScreen(Screen.Valut);
+        }
+
+        private void addedSecInfoButton_Click(object sender, EventArgs e)
+        {
+            //Added
+            int sidx = AlgorithmAddInfoComboBox.SelectedIndex;
+            if (sidx >= 0)
+            {
+                Span<byte> secret = stackalloc byte[Core.Crypto.Base32.GetDecodeLength(CodeAddInfoTextBox.Text)];
+                Core.Crypto.Base32.Decode(CodeAddInfoTextBox.Text, secret);
+                Core.Crypto.AlgorithmType alg = Enum.Parse<AlgorithmType>(AlgorithmAddInfoComboBox.SelectedItem.ToString());
+                Block b = new Block(BlockTypes.TOTP, ((int)DigitsAddInfoNumericUpDown.Value), alg, ((ulong)PeriodAddInfoNumericUpDown.Value), ServiceNameTextBox.Text, secret);
+                _origin.AddBlock(b);
+                _origin.Save();
+                ShowScreen(Screen.Valut);
+            }
+            else 
+            {
+                MessageBox.Show("The algorithm for obtaining the hash is not specified.", "ShotCode", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
